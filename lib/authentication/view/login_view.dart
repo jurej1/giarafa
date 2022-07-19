af0efad0b49c6611169ebdfeac3f10a5fbb48b2f
Jure <jure.04jj@gmail.com>
2022-07-19 18:19:23 +0200

    cleanup

--- lib/authentication/view/login_view.dart
+++ lib/authentication/view/login_view.dart
@@ -25,7 +25,7 @@ class _LoginViewState extends State<LoginView> with SingleTickerProviderStateMix
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 400),
+      duration: const Duration(milliseconds: 300),
       value: 1.0,
     );
   }
@@ -43,41 +43,68 @@ class _LoginViewState extends State<LoginView> with SingleTickerProviderStateMix
     return Stack(
       children: [
         Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 20,
+            horizontal: 10,
+          ),
           height: size.height,
           width: size.width,
           color: Colors.orange,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+              )
+            ],
+          ),
         ),
-        BlocConsumer<DrawerCubit, DrawerState>(
-          listener: (context, state) {
-            if (state.isOpen) {
-              _controller.animateTo(0);
-            } else {
-              _controller.animateBack(1);
-            }
-          },
+        _TopLayer(controller: _controller),
+      ],
+    );
+  }
+}
 
-          //A =1
-          //B= 0
-          // C = 0
-          // D = 100
-          builder: (context, state) {
-            return AnimatedBuilder(
-              animation: _controller,
-              builder: (context, child) {
-                return Transform.translate(
-                  offset: Offset((_controller.value) * -180 + 180, 0),
-                  child: Transform.scale(
-                    scale: ((_controller.value / 1) * (1.0 - 0.8) + 0.8),
-                    child: const Scaffold(
-                      appBar: CustomAppBar(),
-                    ),
-                  ),
-                );
-              },
+class _TopLayer extends StatelessWidget {
+  const _TopLayer({
+    Key? key,
+    required AnimationController controller,
+  })  : _controller = controller,
+        super(key: key);
+
+  final AnimationController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<DrawerCubit, DrawerState>(
+      listener: (context, state) {
+        if (state.isOpen) {
+          _controller.animateTo(0);
+        } else {
+          _controller.animateBack(1);
+        }
+      },
+      builder: (context, state) {
+        return AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset((_controller.value) * -170 + 170, 0),
+              child: Transform.scale(
+                scale: ((_controller.value / 1) * (1.0 - 0.8) + 0.8),
+                child: const Scaffold(
+                  appBar: CustomAppBar(),
+                ),
+              ),
             );
           },
-        ),
-      ],
+        );
+      },
     );
   }
 }
